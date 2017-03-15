28ffa6193c346dfbcaa6966b3182e5dccb7a5a41
13822 <13822@B13-4>
2017-03-15 21:48:44 +0100

    test2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Commentaar");
+            Console.WriteLine("Commentaar2");
            
         }
     }
